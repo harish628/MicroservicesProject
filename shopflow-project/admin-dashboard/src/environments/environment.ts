3bf1a6297b3868aea4fddc6df7422ba3f67ebef9
harish628 <90142836+harish628@users.noreply.github.com>
2026-07-21 18:27:01 +0530

    Adde the /api inplace of localhost

--- shopflow-project/admin-dashboard/src/environments/environment.ts
+++ shopflow-project/admin-dashboard/src/environments/environment.ts
@@ -6,5 +6,5 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000',  // The API Gateway — our ONLY backend address
+  apiUrl: '/api',  // The API Gateway — our ONLY backend address
 };
